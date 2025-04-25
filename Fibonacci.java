079ecbc129a515001dcadbe72ae4ca275c47d288
Priyankashhh <priyankakhandelwal424@gmail.com>
2025-04-25 12:50:37 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,7 +1,7 @@
 public class Fibonacci {
     public static void main(String[] args) {
         int n = 20, first = 0, second = 1;
-        System.out.println("First 20 Fibonacci numbers:");
+        System.out.println("First 20 Fibonacci nos:");
 
         for (int i = 1; i <= n; i++) {
             System.out.print(first + " ");
@@ -9,5 +9,6 @@ public class Fibonacci {
             first = second;
             second = next;
         }
+    
     }
 }
