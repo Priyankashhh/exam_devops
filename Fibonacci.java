1647805cee5f7daca6f3387d27cc3ba86b9fd6fe
devenn05 <rahoolhirlekar@gmail.com>
2025-04-25 12:06:16 +0530

    First commit

---
+++ Fibonacci.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 20, first = 0, second = 1;
+        System.out.println("First 20 Fibonacci numbers:");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
